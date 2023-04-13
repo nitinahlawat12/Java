23a505bc251e42549999d643402dc9a111bf05c7
nitinahlawat12 <130631535+nitinahlawat12@users.noreply.github.com>
2023-04-13 10:16:13 +0530

    Add files via upload

---
+++ ass1.java
@@ -0,0 +1,39 @@
+/*cube of number */
+/* 
+public class ass {
+    public static void main(String[] args){
+        int a=4;
+        int cube=a*a*a;
+        System.out.println("Cube of number : "+cube);
+    }
+    
+}
+*/
+
+/*ascii value */
+/* 
+public class ass{
+    public static void main(String[] args){
+        int a='U';
+        System.out.println(a);
+        int b=(int) a;
+        System.out.println(b);
+
+    }
+}
+*/
+
+/*Swap two number */
+public class ass1{
+    public static void main(String[] args){
+        int a=2;
+        int b=3;
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a + " " + b);
+        
+    }
+}
+
